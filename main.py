dca7b29ddab03d507c6d5fe9c0687a71d2a860be
THBUer-yw <13261059591@163.com>
2020-12-25 16:31:37 +0800

    add tensorboard and save model

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import torch
 import gym
 import argparse
 import os
+import shutil
+from tensorboardX import SummaryWriter
 
 import utils
 import TD3
@@ -27,7 +29,7 @@ def eval_policy(policy, env_name, seed, eval_episodes=10):
 	avg_reward /= eval_episodes
 
 	print("---------------------------------------")
-	print(f"Evaluation over {eval_episodes} episodes: {avg_reward:.3f}")
+	print(f"Evaluation over {eval_episodes} episodes: {avg_reward:.1f}")
 	print("---------------------------------------")
 	return avg_reward
 
@@ -38,9 +40,9 @@ if __name__ == "__main__":
 	parser.add_argument("--policy", default="TD3")                  # Policy name (TD3, DDPG or OurDDPG)
 	parser.add_argument("--env", default="HalfCheetah-v2")          # OpenAI gym environment name
 	parser.add_argument("--seed", default=0, type=int)              # Sets Gym, PyTorch and Numpy seeds
-	parser.add_argument("--start_timesteps", default=25e3, type=int)# Time steps initial random policy is used
+	parser.add_argument("--start_steps", default=25e3, type=int)# Time steps initial random policy is used
 	parser.add_argument("--eval_freq", default=5e3, type=int)       # How often (time steps) we evaluate
-	parser.add_argument("--max_timesteps", default=1e6, type=int)   # Max time steps to run environment
+	parser.add_argument("--total_steps", default=1e6, type=int)   # Max time steps to run environment
 	parser.add_argument("--expl_noise", default=0.1)                # Std of Gaussian exploration noise
 	parser.add_argument("--batch_size", default=256, type=int)      # Batch size for both actor and critic
 	parser.add_argument("--discount", default=0.99)                 # Discount factor
@@ -48,20 +50,33 @@ if __name__ == "__main__":
 	parser.add_argument("--policy_noise", default=0.2)              # Noise added to target policy during critic update
 	parser.add_argument("--noise_clip", default=0.5)                # Range to clip target policy noise
 	parser.add_argument("--policy_freq", default=2, type=int)       # Frequency of delayed policy updates
-	parser.add_argument("--save_model", action="store_true")        # Save model and optimizer parameters
+	parser.add_argument("--save_model", default=True, action="store_true")        # Save model and optimizer parameters
 	parser.add_argument("--load_model", default="")                 # Model load file name, "" doesn't load, "default" uses file_name
 	args = parser.parse_args()
 
-	file_name = f"{args.policy}_{args.env}_{args.seed}"
-	print("---------------------------------------")
-	print(f"Policy: {args.policy}, Env: {args.env}, Seed: {args.seed}")
-	print("---------------------------------------")
+	# file_name = f"{args.policy}_{args.env}_{args.seed}"
+	# print("---------------------------------------")
+	# print(f"Policy: {args.policy}, Env: {args.env}, Seed: {args.seed}")
+	# print("---------------------------------------")
 
 	if not os.path.exists("./results"):
 		os.makedirs("./results")
 
 	if args.save_model and not os.path.exists("./models"):
 		os.makedirs("./models")
+
+	log_save_name = utils.Log_save_name4td3(args)
+	log_save_path = os.path.join("./runs", log_save_name)
+	if os.path.exists(log_save_path):
+		shutil.rmtree(log_save_path)
+
+	writer = SummaryWriter(log_save_path)
+
+	if os.path.exists("./results/" + log_save_name):
+		shutil.rmtree("./results/" + log_save_name)
+	os.makedirs("./results/" + log_save_name)
+
+	log_file = os.path.join("./results/" + log_save_name, "train_log.txt")
 
 	env = gym.make(args.env)
 
@@ -94,9 +109,9 @@ if __name__ == "__main__":
 	elif args.policy == "DDPG":
 		policy = DDPG.DDPG(**kwargs)
 
-	if args.load_model != "":
-		policy_file = file_name if args.load_model == "default" else args.load_model
-		policy.load(f"./models/{policy_file}")
+	# if args.load_model != "":
+	# 	policy_file = file_name if args.load_model == "default" else args.load_model
+	# 	policy.load(f"./models/{policy_file}")
 
 	replay_buffer = utils.ReplayBuffer(state_dim, action_dim)
 	
@@ -107,19 +122,17 @@ if __name__ == "__main__":
 	episode_reward = 0
 	episode_timesteps = 0
 	episode_num = 0
+	max_eval_rewards = -1e6
 
-	for t in range(int(args.max_timesteps)):
+	for t in range(int(args.total_steps)):
 		
 		episode_timesteps += 1
 
 		# Select action randomly or according to policy
-		if t < args.start_timesteps:
+		if t < args.start_steps:
 			action = env.action_space.sample()
 		else:
-			action = (
-				policy.select_action(np.array(state))
-				+ np.random.normal(0, max_action * args.expl_noise, size=action_dim)
-			).clip(-max_action, max_action)
+			action = (policy.select_action(np.array(state))+np.random.normal(0, max_action * args.expl_noise, size=action_dim)).clip(-max_action, max_action)
 
 		# Perform action
 		next_state, reward, done, _ = env.step(action) 
@@ -132,12 +145,15 @@ if __name__ == "__main__":
 		episode_reward += reward
 
 		# Train agent after collecting sufficient data
-		if t >= args.start_timesteps:
+		if t >= args.start_steps:
 			policy.train(replay_buffer, args.batch_size)
 
-		if done: 
+		writer.add_scalar("train/rewrad", episode_reward+1, t+1)
+		writer.add_scalar("train/path_length", episode_timesteps, t+1)
+
+		if done:
 			# +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
-			print(f"Total T: {t+1} Episode Num: {episode_num+1} Episode T: {episode_timesteps} Reward: {episode_reward:.3f}")
+			print(f"current steps: {t+1} path_len: {episode_timesteps} reward: {episode_reward:.1f}")
 			# Reset environment
 			state, done = env.reset(), False
 			episode_reward = 0
@@ -146,6 +162,9 @@ if __name__ == "__main__":
 
 		# Evaluate episode
 		if (t + 1) % args.eval_freq == 0:
-			evaluations.append(eval_policy(policy, args.env, args.seed))
-			np.save(f"./results/{file_name}", evaluations)
-			if args.save_model: policy.save(f"./models/{file_name}")
+			file_name = args.env+"_"+"seed_"+str(args.seed)
+			mean_eval_rewards = eval_policy(policy, args.env, args.seed)
+			writer.add_scalar("eval/mean_reward", mean_eval_rewards, t+1)
+			writer.add_scalar("eval/max_eval_reward", max_eval_rewards, t+1)
+			if mean_eval_rewards > max_eval_rewards and args.save_model:
+				policy.save(f"./models/{file_name}")
