cd49f5753a57b1c0ddea97b0aeec990387c97f2c
THBUer-yw <13261059591@163.com>
2020-12-27 19:50:38 +0800

    update files

--- run_experiments.sh
+++ run_experiments.sh
@@ -11,7 +11,7 @@ CUDA_VISIBLE_DEVICES=0 python3 main.py --seed $seed1 --env "Ant-v2" --policy "TD
 CUDA_VISIBLE_DEVICES=0 python3 main.py --seed $seed2 --env "Ant-v2" --policy "TD3"&
 CUDA_VISIBLE_DEVICES=0 python3 main.py --seed $seed3 --env "Ant-v2" --policy "TD3"&
 CUDA_VISIBLE_DEVICES=0 python3 main.py --seed $seed4 --env "Ant-v2" --policy "TD3"&
-CUDA_VISIBLE_DEVICES=0 python3 main.py --seed $seed5 --env "Ant-v2" --policy "TD3"
+CUDA_VISIBLE_DEVICES=0 python3 main.py --seed $seed5 --env "Ant-v2" --policy "TD3"&
 
 CUDA_VISIBLE_DEVICES=1 python3 main.py --seed $seed1 --env "HalfCheetah-v2" --policy "TD3"&
 CUDA_VISIBLE_DEVICES=1 python3 main.py --seed $seed2 --env "HalfCheetah-v2" --policy "TD3"&
@@ -23,7 +23,7 @@ CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed1 --env "Hopper-v2" --policy 
 CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed2 --env "Hopper-v2" --policy "TD3"&
 CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed3 --env "Hopper-v2" --policy "TD3"&
 CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed4 --env "Hopper-v2" --policy "TD3"&
-CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed5 --env "Hopper-v2" --policy "TD3"
+CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed5 --env "Hopper-v2" --policy "TD3"&
 
 CUDA_VISIBLE_DEVICES=3 python3 main.py --seed $seed1 --env "Reacher-v2" --policy "TD3" --start_steps 1000&
 CUDA_VISIBLE_DEVICES=3 python3 main.py --seed $seed2 --env "Reacher-v2" --policy "TD3" --start_steps 1000&
@@ -35,7 +35,7 @@ CUDA_VISIBLE_DEVICES=0 python3 main.py --seed $seed1 --env "Walker2d-v2" --polic
 CUDA_VISIBLE_DEVICES=0 python3 main.py --seed $seed2 --env "Walker2d-v2" --policy "TD3" --start_steps 5e4&
 CUDA_VISIBLE_DEVICES=0 python3 main.py --seed $seed3 --env "Walker2d-v2" --policy "TD3" --start_steps 5e4&
 CUDA_VISIBLE_DEVICES=0 python3 main.py --seed $seed4 --env "Walker2d-v2" --policy "TD3" --start_steps 5e4&
-CUDA_VISIBLE_DEVICES=0 python3 main.py --seed $seed5 --env "Walker2d-v2" --policy "TD3" --start_steps 5e4
+CUDA_VISIBLE_DEVICES=0 python3 main.py --seed $seed5 --env "Walker2d-v2" --policy "TD3" --start_steps 5e4&
 
 CUDA_VISIBLE_DEVICES=1 python3 main.py --seed $seed1 --env "Humanoid-v2" --policy "TD3"&
 CUDA_VISIBLE_DEVICES=1 python3 main.py --seed $seed2 --env "Humanoid-v2" --policy "TD3"&
@@ -47,7 +47,7 @@ CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed1 --env "InvertedPendulum-v2"
 CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed2 --env "InvertedPendulum-v2" --policy "TD3" --start_steps 1000&
 CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed3 --env "InvertedPendulum-v2" --policy "TD3" --start_steps 1000&
 CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed4 --env "InvertedPendulum-v2" --policy "TD3" --start_steps 1000&
-CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed5 --env "InvertedPendulum-v2" --policy "TD3" --start_steps 1000
+CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed5 --env "InvertedPendulum-v2" --policy "TD3" --start_steps 1000&
 
 CUDA_VISIBLE_DEVICES=3 python3 main.py --seed $seed1 --env "InvertedDoublePendulum-v2" --policy "TD3" --start_steps 1000&
 CUDA_VISIBLE_DEVICES=3 python3 main.py --seed $seed2 --env "InvertedDoublePendulum-v2" --policy "TD3" --start_steps 1000&
