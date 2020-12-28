909448b5578bf6b5184de4bcd471e2a9f8dce16c
THBUer-yw <13261059591@163.com>
2020-12-28 16:31:24 +0800

    rearrange arguments by alphabet

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import utils
 import TD3
 import OurDDPG
 import DDPG
+import gail
 
 
 # Runs policy for X episodes and returns average reward
@@ -37,21 +38,32 @@ def eval_policy(policy, env_name, seed, eval_episodes=10):
 if __name__ == "__main__":
 	
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--policy", default="TD3")                  # Policy name (TD3, DDPG or OurDDPG)
-	parser.add_argument("--env", default="HalfCheetah-v2")          # OpenAI gym environment name
-	parser.add_argument("--seed", default=0, type=int)              # Sets Gym, PyTorch and Numpy seeds
-	parser.add_argument("--start_steps", default=25e3, type=int)  # Time steps initial random policy is used
-	parser.add_argument("--eval_freq", default=5e3, type=int)       # How often (time steps) we evaluate
-	parser.add_argument("--total_steps", default=1e6, type=int)   # Max time steps to run environment
-	parser.add_argument("--expl_noise", default=0.1)                # Std of Gaussian exploration noise
-	parser.add_argument("--batch_size", default=256, type=int)      # Batch size for both actor and critic
-	parser.add_argument("--discount", default=0.99)                 # Discount factor
-	parser.add_argument("--tau", default=0.005)                     # Target network update rate
-	parser.add_argument("--policy_noise", default=0.2)              # Noise added to target policy during critic update
-	parser.add_argument("--noise_clip", default=0.5)                # Range to clip target policy noise
-	parser.add_argument("--policy_freq", default=2, type=int)       # Frequency of delayed policy updates
-	parser.add_argument("--save_model", default=True, action="store_true")        # Save model and optimizer parameters
-	parser.add_argument("--load_model", default="")                 # Model load file name, "" doesn't load, "default" uses file_name
+	parser.add_argument("--batch_size", default=256, type=int, help="Batch size for both actor and critic")
+	parser.add_argument("--discount", default=0.99, help="Discount factor")
+	parser.add_argument("--eval_freq", default=5e3, type=int, help="How often (time steps) we evaluate")
+	parser.add_argument("--env", default="HalfCheetah-v2", help="OpenAI gym environment name")
+	parser.add_argument("--expl_noise", default=0.1, help="Std of Gaussian exploration noise")
+	parser.add_argument('--gail', action='store_true', default=True, help='do imitation learning with gail')
+	parser.add_argument('--gail-batch-size', type=int, default=128, help='gail batch size (default: 128)')
+	parser.add_argument('--gail-experts-dir', default='./gail_experts', help='directory that contains expert demonstrations for gail')
+	parser.add_argument('--gail-epoch', type=int, default=5, help='gail epochs (default: 5)')
+	parser.add_argument('--max_horizon', type=int, default=2048, help='steps interval for training dicriminator')
+	parser.add_argument("--load_model", default="", help="Model load file name")
+	parser.add_argument("--noise_clip", default=0.5, help="Range to clip target policy noise")
+	parser.add_argument('--num_trajs', type=int, default=5)
+	parser.add_argument("--policy", default="TD3", help="Policy name (TD3, DDPG or OurDDPG)")
+	parser.add_argument("--policy_noise", default=0.2, help="Noise added to target policy during critic update")
+	parser.add_argument("--policy_freq", default=2, type=int, help="Frequency of delayed policy updates")
+	parser.add_argument("--reward_type", type=int, default=1, help="different types of rewards")
+	parser.add_argument("--seed", default=0, type=int, help="Sets Gym, PyTorch and Numpy seeds")
+	parser.add_argument("--start_steps", default=2e4, type=int, help="Time steps initial random policy is used")
+	parser.add_argument("--save_model", default=True, action="store_true", help="Save model and optimizer parameters")
+	parser.add_argument('--states_only', type=int, default=0, help="if do imitation learning using only states tuple")
+	parser.add_argument('--subsample_frequency', help='subsample for each trajectory', type=int, default=1)
+	parser.add_argument("--total_steps", default=1e6, type=int, help="Max time steps to run environment")
+	parser.add_argument("--tau", default=0.005, help="Target network update rate")
+	parser.add_argument("--use_lr_decay", default=False, help="decay the learning rate for optimizer")
+	parser.add_argument("--wdail", type=int, default=0, help="train the agent with wdail method")
 	args = parser.parse_args()
 
 	# file_name = f"{args.policy}_{args.env}_{args.seed}"
@@ -81,6 +93,11 @@ if __name__ == "__main__":
 	env.seed(args.seed)
 	torch.manual_seed(args.seed)
 	np.random.seed(args.seed)
+
+	if args.use_lr_decay:
+		lr = utils.LearningRate.get_instance()
+		lr.lr = 10 ** (-3)
+		lr.decay_factor = 0.5
 	
 	state_dim = env.observation_space.shape[0]
 	action_dim = env.action_space.shape[0] 
@@ -119,11 +136,13 @@ if __name__ == "__main__":
 	episode_reward = 0
 	episode_timesteps = 0
 	episode_num = 0
-	max_eval_rewards = -1e6
+	max_eval_rewards = -1e3
+	horizon = 0
 
 	for t in range(int(args.total_steps)):
 		
 		episode_timesteps += 1
+		horizon += 1
 
 		# Select action randomly or according to policy
 		if t < args.start_steps:
@@ -143,7 +162,40 @@ if __name__ == "__main__":
 
 		# Train agent after collecting sufficient data
 		if t >= args.start_steps:
-			policy.train(replay_buffer, args.batch_size)
+			if args.gail and horizon >= args.max_horizon:
+				horizon = 0
+				if args.states_only:
+					discr = gail.Discriminator(kwargs["state_dim"]+kwargs["state_dim"], 100, states_only=args.states_only)
+				else:
+					discr = gail.Discriminator(kwargs["state_dim"]+kwargs["action_dim"], 100, states_only=args.states_only)
+
+				file_name = os.path.join(args.gail_experts_dir, "trajs_{}.pt".format(args.env.split('-')[0].lower()))
+
+				expert_dataset = gail.ExpertDataset(file_name, num_trajectories=args.num_trajs, subsample_frequency=args.subsample_frequency, states_only=args.states_only)
+				args.gail_batch_size = min(args.gail_batch_size, len(expert_dataset))
+				drop_last = len(expert_dataset) > args.gail_batch_size
+				gail_train_loader = torch.utils.data.DataLoader(dataset=expert_dataset, batch_size=args.gail_batch_size, shuffle=True, drop_last=drop_last)
+
+				expert_losses, policy_losses, dis_losses, dis_gps, dis_total_losses = [], [], [], [], []
+				for _ in range(args.gail_epoch):
+					if args.wdail:
+						expert_loss, policy_loss, dis_loss, dis_gp, dis_total_loss = discr.update_wdail(gail_train_loader, replay_buffer)
+					else:
+						expert_loss, policy_loss, dis_loss, dis_gp, dis_total_loss = discr.update(gail_train_loader, replay_buffer)
+
+					expert_losses.append(expert_loss)
+					policy_losses.append(policy_loss)
+					dis_losses.append(dis_loss)
+					dis_gps.append(dis_gp)
+					dis_total_losses.append(dis_total_loss)
+
+				writer.add_scalar("discriminator/expert_loss", np.mean(np.array(expert_losses)), t+1)
+				writer.add_scalar("discriminator/policy_loss", np.mean(np.array(policy_losses)), t+1)
+				writer.add_scalar("discriminator/dis_loss", np.mean(np.array(dis_losses)), t+1)
+				writer.add_scalar("discriminator/dis_gradient", np.mean(np.array(dis_gps)), t+1)
+				writer.add_scalar("discriminator/total_loss", np.mean(np.array(dis_total_losses)), t+1)
+
+			policy.train(args, replay_buffer, discr)
 
 		writer.add_scalar("train/rewrad", episode_reward+1, t+1)
 		writer.add_scalar("train/path_length", episode_timesteps, t+1)
@@ -163,6 +215,6 @@ if __name__ == "__main__":
 			mean_eval_rewards = eval_policy(policy, args.env, args.seed)
 			writer.add_scalar("eval/mean_reward", mean_eval_rewards, t+1)
 			writer.add_scalar("eval/max_eval_reward", max_eval_rewards, t+1)
-			if mean_eval_rewards > max_eval_rewards and args.save_model:
+			if mean_eval_rewards > max_eval_rewards and args.save_model and not args.gail:
 				max_eval_rewards = mean_eval_rewards
 				policy.save(f"./models/{file_name}")
