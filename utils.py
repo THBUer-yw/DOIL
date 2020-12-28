570554264aa440bbedf97ebffabc6aaf347478bb
THBUer-yw <13261059591@163.com>
2020-12-28 16:30:54 +0800

    add learning rate decay class

--- utils.py
+++ utils.py
@@ -41,6 +41,49 @@ class ReplayBuffer(object):
 		)
 
 
+class LearningRate:
+
+	__instance = None
+
+	def __init__(self):
+		if LearningRate.__instance is not None:
+			raise Exception("Singleton instantiation called twice")
+		else:
+			LearningRate.__instance = self
+			self.lr = None
+			self.decay_factor = None
+			self.training_step = 0
+
+	@staticmethod
+	def get_instance():
+		"""Get the singleton instance.
+
+		Returns:
+			(LearningRate)
+		"""
+		if LearningRate.__instance is None:
+			LearningRate()
+		return LearningRate.__instance
+
+	def set_learning_rate(self, lr):
+		self.lr = lr
+
+	def get_learning_rate(self):
+		return self.lr
+
+	def increment_step(self):
+		self.training_step += 1
+
+	def get_step(self):
+		return self.training_step
+
+	def set_decay(self, d):
+		self.decay_factor = d
+
+	def decay(self):
+		self.lr = self.lr * self.decay_factor
+
+
 def Log_save_name4gail(args):
 	time_str = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
 	save_name = args.env_name + '_seed_{}_total_steps_{}_num_trajs_{}_subsample_frequency_{}_prepoch_{}_bcgail_{}_decay_{}_gamma_{}_' \
