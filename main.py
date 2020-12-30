962cd38e63acb9172c25f7417b08dbbe41d2d5ae
THBUer-yw <13261059591@163.com>
2020-12-30 19:46:03 +0800

    update file

--- main.py
+++ main.py
@@ -219,5 +219,5 @@ if __name__ == "__main__":
 			writer.add_scalar("eval/max_eval_reward", max_eval_rewards, t+1)
 			if mean_eval_rewards > max_eval_rewards and args.save_model:
 				max_eval_rewards = mean_eval_rewards
-				if not args.gail:
+				if args.gail == False:
 					policy.save(f"./models/{file_name}")
