3605884d10f00f21c5b566730aed27602184f2f1
THBUer-yw <13261059591@163.com>
2021-05-31 15:29:38 +0800

    run ablation experiments

---
+++ run_ablation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script to reproduce results
+seed1=$RANDOM
+seed2=$RANDOM
+seed3=$RANDOM
+seed4=$RANDOM
+seed5=$RANDOM
+
+# hidden layers 0, 2, 6, 14
+for seed in {$seed1,$seed2,$seed3,$seed4,$seed5}
+do
+  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "Ant-v2" --policy "TD3" --hidden_layers 0&
+  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "BipedalWalker-v3" --policy "TD3" --hidden_layers 0&
+  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "HalfCheetah-v2" --policy "TD3" --hidden_layers 0&
+  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "Hopper-v2" --policy "TD3" --hidden_layers 0&
+  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "Reacher-v2" --policy "TD3" --hidden_layers 0&
+  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "Walker2d-v2" --policy "TD3" --hidden_layers 0
+done
+
+
+
+
