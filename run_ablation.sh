1443e3c334f2b44510266dcc4e5a9ffd09bf048b
THBUer-yw <13261059591@163.com>
2021-05-31 15:35:34 +0800

    update files

--- run_ablation.sh
+++ run_ablation.sh
@@ -10,12 +10,12 @@ seed5=$RANDOM
 # hidden layers 0, 2, 6, 14
 for seed in {$seed1,$seed2,$seed3,$seed4,$seed5}
 do
-  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "Ant-v2" --policy "TD3" --hidden_layers 0&
-  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "BipedalWalker-v3" --policy "TD3" --hidden_layers 0&
-  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "HalfCheetah-v2" --policy "TD3" --hidden_layers 0&
-  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "Hopper-v2" --policy "TD3" --hidden_layers 0&
-  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "Reacher-v2" --policy "TD3" --hidden_layers 0&
-  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "Walker2d-v2" --policy "TD3" --hidden_layers 0
+  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "Ant-v2" --hidden_layers 0&
+  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "BipedalWalker-v3" --hidden_layers 0&
+  CUDA_VISIBLE_DEVICES=1 python main.py --seed $seed --env "HalfCheetah-v2" --hidden_layers 0&
+  CUDA_VISIBLE_DEVICES=1 python main.py --seed $seed --env "Hopper-v2" --hidden_layers 0&
+  CUDA_VISIBLE_DEVICES=2 python main.py --seed $seed --env "Reacher-v2" --hidden_layers 0&
+  CUDA_VISIBLE_DEVICES=2 python main.py --seed $seed --env "Walker2d-v2" --hidden_layers 0
 done
 
 
--- run_experiments.sh
+++ run_experiments.sh
@@ -7,18 +7,14 @@ seed3=$RANDOM
 seed4=$RANDOM
 seed5=$RANDOM
 
-
 for seed in {$seed1,$seed2,$seed3,$seed4,$seed5}
 do
-  for reward in {1,2,3,4,5,6,7,8}
-  do
-    CUDA_VISIBLE_DEVICES=1 python3 main.py --seed $seed --env "Ant-v2" --policy "TD3" --reward_type $reward --wdail 0&
-    CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed --env "HalfCheetah-v2" --policy "TD3" --reward_type $reward --wdail 0&
-    CUDA_VISIBLE_DEVICES=3 python3 main.py --seed $seed --env "Hopper-v2" --policy "TD3" --reward_type $reward --wdail 0&
-    CUDA_VISIBLE_DEVICES=1 python3 main.py --seed $seed --env "Reacher-v2" --policy "TD3" --reward_type $reward --wdail 0&
-    CUDA_VISIBLE_DEVICES=2 python3 main.py --seed $seed --env "Walker2d-v2" --policy "TD3" --reward_type $reward --wdail 0&
-    CUDA_VISIBLE_DEVICES=3 python3 main.py --seed $seed --env "BipedalWalker-v3" --policy "TD3" --reward_type $reward --wdail 0
-  done
+  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "Ant-v2" --use_dense_network 1&
+  CUDA_VISIBLE_DEVICES=0 python main.py --seed $seed --env "BipedalWalker-v3" --use_dense_network 1&
+  CUDA_VISIBLE_DEVICES=1 python main.py --seed $seed --env "HalfCheetah-v2" --use_dense_network 1&
+  CUDA_VISIBLE_DEVICES=1 python main.py --seed $seed --env "Hopper-v2" --use_dense_network 1&
+  CUDA_VISIBLE_DEVICES=2 python main.py --seed $seed --env "Reacher-v2" --use_dense_network 1&
+  CUDA_VISIBLE_DEVICES=2 python main.py --seed $seed --env "Walker2d-v2" --use_dense_network 1
 done
 
 
